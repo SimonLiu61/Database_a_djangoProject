755a2fa5a6d6ac57e71f1a43a72b055558742305
CaesarYang <CaesarYangs@outlook.com>
2021-10-05 18:43:26 +0800

    10.5 机场系统部分基本完成

--- airport/urls.py
+++ airport/urls.py
@@ -5,6 +5,10 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^airportmanage', views.airportManagement),
+    url(r'^markdelay', views.sendDelayMessage),
+    url(r'^canceldelay', views.cancelDelayMessage),
+    url(r'^cancelflight', views.cancelFlight),
+    url(r'^ticketmanage', views.ticketmanage),
 
     # url(r'^myticket/$', views),
 ]
