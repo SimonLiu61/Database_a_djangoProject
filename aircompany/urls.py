3d5f66aa317ba27a744a942812b29cbe75123984
CaesarYang <CaesarYangs@outlook.com>
2021-10-05 20:04:30 +0800

    10.5 航司部分修改航线部分完成

--- aircompany/urls.py
+++ aircompany/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^manage', views.companyManage),
+    url(r'^ticketmanage', views.ticketmanage),
 
     # url(r'^myticket/$', views),
 ]
